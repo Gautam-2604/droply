2e514a180517e9607c23447947a7497686c045b3
Gautam-2604 <gaurisariag@gmail.com>
2025-05-11 08:34:34 +0530

    drizzle completed and zod added

---
+++ lib/db/index.ts
@@ -0,0 +1,10 @@
+import { drizzle  } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "./schema";
+
+const sql = neon(process.env.DATABASE_URLL!);
+
+export const db = drizzle(sql, {schema})
+export {sql}
+ 
